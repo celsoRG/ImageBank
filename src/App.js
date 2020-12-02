f9217de7b573a2561dd128d2b43e9b5409307708
celsoRG <celso.re.go@hotmail.com>
2020-12-01 22:47:42 -0600

    Se  creafunci'on que recibe datos desde el componente hijo buscador

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, {Component} from 'react';
 import Buscador from './components/Buscador'
 
 class App extends  Component {
+
+  //Funcio'n que recibe datos enviados por componentes hijos
+  datosBusqueda = (termino) =>{
+    console.log(termino);
+  }
+
   render(){
     return (
       <div className="App container">
         <div className = "jumbotron">
           <p className ="lead text-center">Buscador de imagenes</p>
-          <Buscador/>
+          <Buscador
+           datosBusqueda = {this.datosBusqueda}
+          />
         </div>
       
       </div>
--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 
 
 class Buscador extends Component {
-    state = {  }
+    //funci[on que permite leer valores de inputs
+    //mediante react y se indica donde se va utilizar 
+    busquedaRef = React.createRef();
+
+    handleGetData = (e) => {
+        e.preventDefault();
+        //se extrae el contenido del input o bien se toma el valor del input
+        //console.log(this.busquedaRef.current.value);
+        const termino = this.busquedaRef.current.value;
+
+        //se envia el valor al componente padre
+        this.props.datosBusqueda(termino);
+
+    }
     render() { 
-        return ( 
-            <form>
+        return ( //El onSubmit para atrapar el evento del click en el boto'n buscar y que debe de hacer
+            <form onSubmit = {this.handleGetData}> 
                 <div className= "row">
+            
                     <div className= "form-group col-md-8">
-                            <input 
+                            <input //Se indica que aqui se utiliza la funcio'n ref
+                                ref = {this.busquedaRef} 
                                 type="text" 
                                 className="form-control form-control-lg" 
                                 placeholder ="Busca tu imagen. Ejemplo: Futbol"
